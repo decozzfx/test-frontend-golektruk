8ba1a7b0cf65db85d9ddaeea7f4d2a126d0ad331
decozzfx <m.f.r.ozil@gmail.com>
2024-11-12 07:41:12 +0700

    feat(soal1): dragable component

--- src/soal/soal1.tsx
+++ src/soal/soal1.tsx
@@ -1,29 +1,90 @@
-
+import React, { useCallback, useState } from "react";
 
 const Soal1 = () => {
   // 1. Buat kotak dibawah menjadi elemen drag and drop tanpa menggunakan plugin
 
+  const DraggableComponent = () => {
+    const [isDragging, setIsDragging] = useState(false);
+
+    const [position, setPosition] = useState({ x: 100, y: 100 });
+
+    const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+      setIsDragging(true);
+
+      setOffset({
+        x: e.clientX - position.x,
+
+        y: e.clientY - position.y,
+      });
+    };
+
+    const handleMouseMove = useCallback(
+      (e: { clientX: number; clientY: number }) => {
+        if (isDragging) {
+          setPosition({
+            x: e.clientX - offset.x,
+
+            y: e.clientY - offset.y,
+          });
+        }
+      },
+      [isDragging, offset]
+    );
+
+    const handleMouseUp = () => {
+      setIsDragging(false);
+    };
+
+    React.useEffect(() => {
+      if (isDragging) {
+        window.addEventListener("mousemove", handleMouseMove);
+        window.addEventListener("mouseup", handleMouseUp);
+      } else {
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
+      }
+
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
+      };
+    }, [handleMouseMove, isDragging]);
+
+    return (
+      <div
+        className="draggable"
+        onMouseDown={handleMouseDown}
+        style={{
+          position: "absolute",
+          left: position.x,
+          top: position.y,
+          width: "40px",
+          height: "40px",
+          backgroundColor: "white",
+          cursor: "grab",
+          borderRadius: "8px",
+        }}
+      ></div>
+    );
+  };
 
   return (
-      <>
-        <div style={{
-          backgroundColor: '#fff',
-          width: 40,
-          height: 40,
-          borderRadius:"8px",
+    <>
+      <DraggableComponent />
+      {/* Ekspektasi hasil */}
+      <iframe
+        src="/soal1.mp4"
+        style={{
+          position: "fixed",
+          bottom: 0,
+          right: 0,
+          border: "1px solid white",
         }}
-        >
-        </div>
+      ></iframe>
+    </>
+  );
+};
 
-        {/* Ekspektasi hasil */}
-        <iframe src="/soal1.mp4" style={{
-          position:"fixed",
-          bottom:0,
-          right:0,
-          border:"1px solid white",
-        }}></iframe>
-      </>
-  )
-}
-
-export default Soal1
+export default Soal1;
